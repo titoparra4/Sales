1d429deb1151b1238d50113decdebc94f3ec953e
titoparra4 <tito.parra4@hotmail.com>
2018-10-22 08:25:34 -0500

    Update Sales/Sales/ViewModels/EditProductViewModel.cs

--- Sales/Sales/ViewModels/EditProductViewModel.cs
+++ Sales/Sales/ViewModels/EditProductViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sales.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,11 @@ namespace Sales.ViewModels
 {
     public class EditProductViewModel
     {
+        private Product product;
+
+        public EditProductViewModel(ProductItemViewModel productItemViewModel)
+        {
+            this.product = productItemViewModel;
+        }
     }
 }

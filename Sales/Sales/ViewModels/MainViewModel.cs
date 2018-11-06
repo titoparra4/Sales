0624a9db3c8eeac6e4e6a92943582b648dca0643
titoparra4 <tito.parra4@hotmail.com>
2018-11-06 17:06:29 -0500

    Imageuser

--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -39,18 +39,28 @@ namespace Sales.ViewModels
             }
         }
 
+
         public string UserImageFullPath
         {
             get
             {
-                if (this.UserASP != null && this.UserASP.Claims != null && this.UserASP.Claims.Count > 3)
+                foreach (var claim in this.UserASP.Claims)
                 {
-                    return $"https://salesapitito.azurewebsites.net{this.UserASP.Claims[3].ClaimValue.Substring(1)}";
+                    if (claim.ClaimType == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri")
+                    {
+                        if (claim.ClaimValue.StartsWith("~"))
+                        {
+                            return $"https://salesapitito.azurewebsites.net{claim.ClaimValue.Substring(1)}";
+                        }
+
+                        return claim.ClaimValue;
+                    }
                 }
 
                 return null;
             }
         }
+
 
         #endregion
 

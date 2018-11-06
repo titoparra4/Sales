759c3bb00551997085995922ba3e017067bde79a
titoparra4 <tito.parra4@hotmail.com>
2018-11-06 15:40:11 -0500

    Spalsh

--- Sales/Sales.Android/Resources/Resource.designer.cs
+++ Sales/Sales.Android/Resources/Resource.designer.cs
@@ -2342,26 +2342,26 @@ namespace Sales.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_1 = 2130837819;
-			
 			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_2 = 2130837820;
+			public const int avd_hide_password_1 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_3 = 2130837821;
+			public const int avd_hide_password_2 = 2130837821;
+			
+			// aapt resource value: 0x7f02013e
+			public const int avd_hide_password_3 = 2130837822;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_1 = 2130837822;
-			
 			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_2 = 2130837823;
+			public const int avd_show_password_1 = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int avd_show_password_3 = 2130837824;
+			public const int avd_show_password_2 = 2130837824;
+			
+			// aapt resource value: 0x7f020141
+			public const int avd_show_password_3 = 2130837825;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -3032,11 +3032,11 @@ namespace Sales.Droid
 			// aapt resource value: 0x7f020133
 			public const int notification_icon_background = 2130837811;
 			
-			// aapt resource value: 0x7f020139
-			public const int notification_template_icon_bg = 2130837817;
-			
 			// aapt resource value: 0x7f02013a
-			public const int notification_template_icon_low_bg = 2130837818;
+			public const int notification_template_icon_bg = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int notification_template_icon_low_bg = 2130837819;
 			
 			// aapt resource value: 0x7f020134
 			public const int notification_tile_bg = 2130837812;
@@ -3048,10 +3048,13 @@ namespace Sales.Droid
 			public const int nouser = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int tooltip_frame_dark = 2130837815;
+			public const int splash = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int tooltip_frame_light = 2130837816;
+			public const int tooltip_frame_dark = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int tooltip_frame_light = 2130837817;
 			
 			static Drawable()
 			{
@@ -5108,6 +5111,9 @@ namespace Sales.Droid
 			
 			// aapt resource value: 0x7f0d0006
 			public const int Theme_MediaRouter_LightControlPanel = 2131558406;
+			
+			// aapt resource value: 0x7f0d0192
+			public const int Theme_Splash = 2131558802;
 			
 			// aapt resource value: 0x7f0d0116
 			public const int ThemeOverlay_AppCompat = 2131558678;

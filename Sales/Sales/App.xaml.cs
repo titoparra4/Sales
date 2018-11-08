ecd1727f19ced077ab814b428825077b5af345df
titoparra4 <tito.parra4@hotmail.com>
2018-11-07 23:55:13 -0500

    LoginFacebook

--- Sales/Sales/App.xaml.cs
+++ Sales/Sales/App.xaml.cs
@@ -10,6 +10,8 @@ namespace Sales
     using Sales.Helpers;
     using Newtonsoft.Json;
     using Sales.Common.Models;
+    using System.Threading.Tasks;
+    using Sales.Services;
 
     public partial class App : Application
 	{
@@ -40,6 +42,43 @@ namespace Sales
 
 
         }
+
+        public static Action HideLoginView
+        {
+            get
+            {
+                return new Action(() => Current.MainPage = new NavigationPage(new LoginPage()));
+            }
+        }
+
+        public static async Task NavigateToProfile(TokenResponse token)
+        {
+            if (token == null)
+            {
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
+                return;
+            }
+
+            Settings.IsRemembered = true;
+            Settings.AccessToken = token.AccessToken;
+            Settings.TokenType = token.TokenType;
+
+            var apiService = new ApiService();
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlUsersController"].ToString();
+            var response = await apiService.GetUser(url, prefix, $"{controller}/GetUser", token.UserName, token.TokenType, token.AccessToken);
+            if (response.IsSuccess)
+            {
+                var userASP = (MyUserASP)response.Result;
+                MainViewModel.GetInstance().UserASP = userASP;
+                Settings.UserASP = JsonConvert.SerializeObject(userASP);
+            }
+
+            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            Application.Current.MainPage = new MasterPage();
+        }
+
 
         protected override void OnStart ()
 		{

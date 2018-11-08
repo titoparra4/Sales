cbbbf2fd3dde0bf1e211a02835b33862e695e3df
titoparra4 <tito.parra4@hotmail.com>
2018-11-08 16:25:49 -0500

    CategoryII

---
+++ Sales.Backend/Models/CategoryView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales.Backend.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+    using Common.Models;
+
+    public class CategoryView : Category
+    {
+        [Display(Name = "Image")]
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+
+}
